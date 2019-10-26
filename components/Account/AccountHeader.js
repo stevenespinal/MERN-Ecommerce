0b870802638daf5d92a0b35019ba451ef6ee04a0
Steven Espinal <stevenjesusespinal@gmail.com>
2019-10-26 00:39:36 -0400

    added changes to the account page after logging into your account

--- components/Account/AccountHeader.js
+++ components/Account/AccountHeader.js
@@ -1,5 +1,20 @@
-function AccountHeader() {
-  return <>AccountHeader</>;
+import {Header, Icon, Segment, Label} from 'semantic-ui-react';
+import React, {Fragment} from "react";
+
+function AccountHeader({role, email, name, createdAt}) {
+  return (
+    <Fragment>
+      <Segment secondary inverted color="violet">
+        <Label color="teal" size="large" ribbon icon="privacy" style={{textTransform: "capitalize"}} content={role}/>
+        <Header inverted textAlign="center" as="h1" icon>
+          <Icon name="user"/>
+          {name}
+          <Header.Subheader>{email}</Header.Subheader>
+          <Header.Subheader>Joined {createdAt}</Header.Subheader>
+        </Header>
+      </Segment>
+    </Fragment>
+  );
 }
 
 export default AccountHeader;
--- pages/account.js
+++ pages/account.js
@@ -1,5 +1,16 @@
-function Account() {
-  return <>account</>;
+import AccountHeader from "../components/Account/AccountHeader";
+import AccountOrders from "../components/Account/AccountOrders";
+import React, {Fragment} from 'react';
+
+
+function Account({user}) {
+  return (
+    <Fragment>
+      <AccountHeader {...user}/>
+      <AccountOrders/>
+    </Fragment>
+  );
 }
+
 
 export default Account;
